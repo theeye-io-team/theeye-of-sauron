7b46a5399ef006cd2cef7e1bd713bdaaa0b1d992
facugon <facugon@theeye.io>
2021-11-09 21:19:35 -0300

    added CWD to the script (to ensure used paths)

--- quickstart.sh
+++ quickstart.sh
@@ -5,38 +5,43 @@ echo "# cloning repos for building images"
 echo "#"
 echo "#"
 
+cwd="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 echo "## WEB"
-if [ -d ./sources/theeye-web ]
+if [ -d ${cwd}/sources/theeye-web ]
 then
-  cd ./sources/theeye-web
+  cd ${cwd}/sources/theeye-web
   git fetch
   git reset --hard origin/master
-  cd ..
 else
   git clone --recursive https://github.com/theeye-io-team/theeye-web.git ./sources/theeye-web
 fi
 
+cd ${cwd}
+
 echo "## SUPERVISOR"
-if [ -d ./sources/theeye-supervisor ]
+if [ -d ${cwd}/sources/theeye-supervisor ]
 then
-  cd ./sources/theeye-supervisor
+  cd ${cwd}/sources/theeye-supervisor
   git fetch
   git reset --hard origin/master
-  cd ..
 else
   git clone --recursive https://github.com/theeye-io-team/theeye-supervisor.git ./sources/theeye-supervisor
 fi
 
+cd ${cwd}
+
 echo "## GATEWAY"
-if [ -d ./sources/theeye-gateway ]
+if [ -d ${cwd}/sources/theeye-gateway ]
 then
-  cd ./sources/theeye-gateway
+  cd ${cwd}/sources/theeye-gateway
   git fetch
   git reset --hard origin/master
-  cd ..
 else
   git clone --recursive https://github.com/theeye-io-team/theeye-gateway.git ./sources/theeye-gateway
 fi
+
+cd ${cwd}
 
 echo "################"
 echo "#"
@@ -50,14 +55,25 @@ echo "#"
 echo "# building local images"
 echo "#"
 echo "################"
-docker-compose -f quickstart.yml build
+
+docker-compose -f ${cwd}/quickstart.yml down
+rm -rf ${cwd}/web
+
+#docker images | grep "theeye-" | cut -d" " -f1 | while read image
+#do
+#  docker rmi ${image}
+#done
+
+cd ${cwd}
+docker-compose -f ${cwd}/quickstart.yml build
 
 echo "################"
 echo "#"
 echo "# copying assets"
 echo "#"
 echo "################"
-docker run --rm -dit --name 'theeye-web-export' -v ${PWD}/web:/output 'theeye-io/theeye-web' cp -r /app/dist/. /output;
+cd ${cwd}
+docker run --rm -dit --name 'theeye-web-export' -v ${cwd}/web:/output 'theeye-io/theeye-web' cp -r /app/dist/. /output;
 
 echo "################"
 echo "#"
@@ -65,13 +81,15 @@ echo "# starting engines"
 echo "#"
 echo "################"
 #docker-compose -f quickstart.yml up -d theeye-redis theeye-db
-docker-compose -f quickstart.yml up -d
+cd ${cwd}
+docker-compose -f ${cwd}/quickstart.yml up -d
 
 echo "################"
 echo "#"
 echo "# importing database"
 echo "#"
 echo "################"
+cd ${cwd}
 tar -xzf theeye-mongodb.tgz
 docker exec -it theeye-mongodb mongorestore /data/dump/
 

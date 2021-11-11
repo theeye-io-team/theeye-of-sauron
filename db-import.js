788cb7df18148a76223b2ae7d67fc25cd75a78c7
Facundo <facugon@theeye.io>
2021-11-11 10:02:29 -0300

    code origin / references

--- db-import.js
+++ db-import.js
@@ -1,3 +1,4 @@
+// https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
 function makeid(length) {
   var result           = '';
   var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
